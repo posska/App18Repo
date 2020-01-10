b64316ea89429ffdd2f23f031b54c15fbdd9cdd5
posska <47723973+posska@users.noreply.github.com>
2020-01-09 21:31:53 -0700

    Adding vs files

---
+++ App18/App18/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App18
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+         private void Button_Enter_OnClick(object sneder, EventArgs e)
+        {
+            Label_Display.Text = "Changed";
+        }
+    }
+}
